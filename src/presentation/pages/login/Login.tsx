8271ad5df7715b410aacd7b877eba6ce9b94f304
aleksandersousa <aleksander-ssousa@outlook.com>
2023-04-24 21:36:16 -0300

    feat: ensure Login starts with button disabled

--- src/presentation/pages/login/Login.tsx
+++ src/presentation/pages/login/Login.tsx
@@ -26,7 +26,7 @@ const Login: React.FC = () => {
             <Input type="password" name="password" placeholder="Digite sua senha" />
           </div>
 
-          <button className={Styles.submit} type="submit">
+          <button disabled data-testid="submit" className={Styles.submit} type="submit">
             Entrar
           </button>
 
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,4 +9,11 @@ describe('Login Page', () => {
     const errorWrap = getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
   });
+
+  test('should show submit button disabled on start', () => {
+    const { getByTestId } = render(<Login />);
+
+    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
+  });
 });
