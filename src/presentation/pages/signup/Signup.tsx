c1617be4602dcc21f96c7b13d2d41dc08e3c031e
aleksandersousa <aleksander-ssousa@outlook.com>
2023-04-29 17:53:59 -0300

    feat: ensure Signup starts with correct initial states

--- src/presentation/pages/signup/Signup.tsx
+++ src/presentation/pages/signup/Signup.tsx
@@ -1,44 +1,57 @@
 /* eslint-disable object-curly-newline */
 /* eslint-disable react/jsx-no-constructed-context-values */
-import React from 'react';
+import React, { useState } from 'react';
 import { Footer, FormStatus, Input, PublicHeader } from '@/presentation/components';
 import FormContext from '@/presentation/contexts/form/form-context';
 import { Link } from 'react-router-dom';
 import Styles from './styles.scss';
 
-const Signup: React.FC = () => (
-  <div className={Styles.signup}>
-    <PublicHeader />
+const Signup: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    email: '',
+    password: '',
+    nameError: 'Campo obrigatorio',
+    emailError: 'Campo obrigatorio',
+    passwordError: 'Campo obrigatorio',
+    passwordConfirmationError: 'Campo obrigatorio',
+    mainError: '',
+  });
 
-    <FormContext.Provider value={{ state: {} }}>
-      <form className={Styles.form}>
-        <h2>Criar Conta</h2>
+  return (
+    <div className={Styles.signup}>
+      <PublicHeader />
 
-        <div className={Styles.inputWrap}>
-          <Input type="text" name="name" placeholder="Digite seu nome" />
-          <Input type="email" name="email" placeholder="Digite seu e-mail" />
-          <Input type="password" name="password" placeholder="Digite sua senha" />
-          <Input
-            type="password"
-            name="passwordConfirmation"
-            placeholder="Repita sua senha"
-          />
-        </div>
+      <FormContext.Provider value={{ state }}>
+        <form className={Styles.form}>
+          <h2>Criar Conta</h2>
 
-        <button type="submit" data-testid="submit" className={Styles.submit}>
-          Criar
-        </button>
+          <div className={Styles.inputWrap}>
+            <Input type="text" name="name" placeholder="Digite seu nome" />
+            <Input type="email" name="email" placeholder="Digite seu e-mail" />
+            <Input type="password" name="password" placeholder="Digite sua senha" />
+            <Input
+              type="password"
+              name="passwordConfirmation"
+              placeholder="Repita sua senha"
+            />
+          </div>
 
-        <Link to="/login" className={Styles.link}>
-          Voltar para Login
-        </Link>
+          <button type="submit" data-testid="submit" className={Styles.submit} disabled>
+            Criar
+          </button>
 
-        <FormStatus />
-      </form>
-    </FormContext.Provider>
+          <Link to="/login" className={Styles.link}>
+            Voltar para Login
+          </Link>
 
-    <Footer />
-  </div>
-);
+          <FormStatus />
+        </form>
+      </FormContext.Provider>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Signup;
