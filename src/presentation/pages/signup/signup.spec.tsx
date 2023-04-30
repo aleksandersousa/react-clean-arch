57bc13de2dd56428d0b57a841a5695927b861a09
aleksandersousa <aleksander-ssousa@outlook.com>
2023-04-30 15:47:56 -0300

    test: ensure Signup shows spinner on valid submit

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { RenderResult, cleanup, render } from '@testing-library/react';
+import {
+  RenderResult,
+  cleanup,
+  fireEvent,
+  render,
+  waitFor,
+} from '@testing-library/react';
 import {
   AuthenticationSpy,
   Helper,
@@ -34,6 +40,23 @@ const makeSut = (params?: SutParams): SutTypes => {
   );
 
   return { sut, authenticationSpy, saveAccessTokenMock };
+};
+
+const simulateValidSubmit = async (
+  sut: RenderResult,
+  name = faker.definitions.name.first_name[0],
+  email = faker.internet.email(),
+  password = faker.internet.password()
+): Promise<void> => {
+  Helper.populateField(sut, 'name', name);
+  Helper.populateField(sut, 'email', email);
+  Helper.populateField(sut, 'password', password);
+  Helper.populateField(sut, 'passwordConfirmation', password);
+
+  const form = sut.getByTestId('form');
+  fireEvent.submit(form);
+
+  await waitFor(() => form);
 };
 
 describe('Signup Page', () => {
@@ -134,5 +157,13 @@ describe('Signup Page', () => {
     Helper.populateField(sut, 'passwordConfirmation');
 
     Helper.testButtonIsDisabled(sut, 'submit', false);
+  });
+
+  test('should show spinner on submit', async () => {
+    const { sut } = makeSut();
+
+    await simulateValidSubmit(sut);
+
+    Helper.testElementExists(sut, 'spinner');
   });
 });
