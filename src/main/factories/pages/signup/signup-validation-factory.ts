8b3d30ef47a85501d44a2b37155ba275039bf24d
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-03 11:17:01 -0300

    feat: add Signup Validation factory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,10 @@
+import { ValidationBuilder as Builder } from '@/validation/validators';
+import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite';
+
+export const makeSignupValidation = (): ValidationComposite =>
+  ValidationComposite.build([
+    ...Builder.field('name').required().min(5).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().min(5).build(),
+    ...Builder.field('passwordConfirmation').required().min(5).build(),
+  ]);
