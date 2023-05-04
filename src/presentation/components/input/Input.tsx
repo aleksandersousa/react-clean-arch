9d312ca7cb473c0dc0244fda9ebc101a7db674e9
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-04 15:25:09 -0300

    refactor: add input animations

--- src/presentation/components/input/Input.tsx
+++ src/presentation/components/input/Input.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import formContext from '@/presentation/contexts/form/form-context';
 import Styles from './styles.scss';
 
@@ -11,27 +11,35 @@ type Props = React.DetailedHTMLProps<
 
 const Input: React.FC<Props> = ({ type, name, placeholder }: Props) => {
   const { state, setState } = useContext(formContext);
+  const inputRef = useRef<HTMLInputElement>(null);
   const error = state[`${name}Error`] as string;
-
-  const getStatus = (): string => (error ? '🔴' : '🟢');
-
-  const getTitle = (): string => error || 'Tudo certo!';
-
-  const handleChange = (e?: React.ChangeEvent<HTMLInputElement>) => {
-    setState((prev: any) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
 
   return (
     <div className={Styles.inputWrap}>
       <input
+        placeholder=" "
+        ref={inputRef}
         data-testid={name}
         type={type}
         name={name}
-        placeholder={placeholder}
-        onChange={handleChange}
+        onChange={e => {
+          setState((prev: any) => ({ ...prev, [e.target.name]: e.target.value }));
+        }}
       />
-      <span data-testid={`${name}-status`} className={Styles.status} title={getTitle()}>
-        {getStatus()}
+      <label
+        data-testid={`${name}-label`}
+        onClick={() => {
+          inputRef.current.focus();
+        }}
+      >
+        {placeholder}
+      </label>
+      <span
+        data-testid={`${name}-status`}
+        className={Styles.status}
+        title={error || 'Tudo certo!'}
+      >
+        {error ? '🔴' : '🟢'}
       </span>
     </div>
   );
