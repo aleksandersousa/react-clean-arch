3a28554fa70e7968a1a087511a742def7414706c
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-05 22:27:48 -0300

    test: ensure Signup loads with correct state

---
+++ src/main/test/cypress/integration/signup.cy.ts
@@ -0,0 +1,16 @@
+import { testInputStatus } from '../support/form-helper';
+
+describe('Signup', () => {
+  beforeEach(() => cy.visit('/signup'));
+
+  it('Should load with correct initial state', () => {
+    testInputStatus('name', 'Campo obrigatório');
+    testInputStatus('email', 'Campo obrigatório');
+    testInputStatus('password', 'Campo obrigatório');
+    testInputStatus('passwordConfirmation', 'Campo obrigatório');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+});
