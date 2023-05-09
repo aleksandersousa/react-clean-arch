02a1ae97a1457af9e7049e31670b6c2166e5bcac
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-09 11:09:00 -0300

    feat: ensure AxiosHttpClient calls axios.get with correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,4 +1,9 @@
-import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http';
+import {
+  HttpGetParams,
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse,
+} from '@/data/protocols/http';
 import axios, { AxiosResponse } from 'axios';
 
 export class AxiosHttpClient implements HttpPostClient {
@@ -7,6 +12,21 @@ export class AxiosHttpClient implements HttpPostClient {
 
     try {
       axiosResponse = await axios.post(params.url, params.body);
+    } catch (error) {
+      axiosResponse = error.response;
+    }
+
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    };
+  }
+
+  async get(params: HttpGetParams): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse;
+
+    try {
+      axiosResponse = await axios.get(params.url);
     } catch (error) {
       axiosResponse = error.response;
     }
