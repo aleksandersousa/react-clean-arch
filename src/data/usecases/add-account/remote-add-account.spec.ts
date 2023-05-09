d4763cb4f929c30895712a80fede7aff5bf11d6f
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-09 08:34:03 -0300

    refactor: remove generic body type of HttpPostClient

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -1,5 +1,4 @@
 import { HttpPostClientSpy } from '@/data/test';
-import { AddAccountParams } from '@/domain/usecases';
 import { AccountModel } from '@/domain/models';
 import { faker } from '@faker-js/faker';
 import { mockAccountModel, mockAddAccountParams } from '@/domain/test';
@@ -9,11 +8,11 @@ import { RemoteAddAccount } from './remote-add-account';
 
 type SutTypes = {
   sut: RemoteAddAccount;
-  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>;
+  httpPostClientSpy: HttpPostClientSpy<AccountModel>;
 };
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy<AddAccountParams, AccountModel>();
+  const httpPostClientSpy = new HttpPostClientSpy<AccountModel>();
   const sut = new RemoteAddAccount(url, httpPostClientSpy);
 
   return { sut, httpPostClientSpy };
