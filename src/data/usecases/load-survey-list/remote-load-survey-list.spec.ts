21f77f70345ff718dee19e7a20deb73f1360becc
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-09 09:31:02 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 500

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -48,4 +48,15 @@ describe('RemoteLoadSurveyList', () => {
 
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test('should throw UnexpectedError if HTTPGetClient return 404', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+
+    const promise = sut.loadAll();
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
