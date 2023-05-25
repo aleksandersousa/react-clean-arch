3d203b3c2027fd9a750448db1dc7a45c148b82a7
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-25 02:12:48 -0300

    refactor: move icon to its own file

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -6,3 +6,4 @@ export { default as Input } from './input/Input';
 export { default as FormStatus } from './formStatus/FormStatus';
 export { default as SubmitButton } from './submitButton/SubmitButton';
 export { default as Header } from './header/Header';
+export { default as Icon, IconName } from './icon/Icon';
