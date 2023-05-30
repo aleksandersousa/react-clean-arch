79085f74ed56ca2fa73dc84553432e5a371bdadc
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-30 10:58:54 -0300

    test: ensure survey item loads survey data correctly

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -1,20 +1,24 @@
 import { faker } from '@faker-js/faker';
 import { SurveyModel } from '../models';
 
+export const mockSurveyModel = (): SurveyModel => ({
+  id: faker.string.uuid(),
+  question: faker.word.words(10),
+  answers: [
+    {
+      image: faker.internet.avatar(),
+      answer: faker.word.words(4),
+    },
+    {
+      answer: faker.word.words(5),
+    },
+  ],
+  didAnswer: faker.datatype.boolean(),
+  date: faker.date.recent(),
+});
+
 export const mockSurveyListModel = (): SurveyModel[] => [
-  {
-    id: faker.string.uuid(),
-    question: faker.word.words(10),
-    answers: [
-      {
-        image: faker.internet.avatar(),
-        answer: faker.word.words(4),
-      },
-      {
-        answer: faker.word.words(5),
-      },
-    ],
-    didAnswer: faker.datatype.boolean(),
-    date: faker.date.recent(),
-  },
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel(),
 ];
