72a1e9804bb715a2fabb104487fa71b2f55e9df8
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-30 10:17:12 -0300

    test: ensure SurveyLisy renders 4 empty LIs on start

---
+++ src/presentation/pages/survey-list/surveyList.spec.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import SurveyList from './SurveyList';
+
+describe('SurveyList Component', () => {
+  test('should present 4 empty items on start', () => {
+    render(<SurveyList />);
+
+    const surveyList = screen.getByTestId('survey-list');
+
+    expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+  });
+});
