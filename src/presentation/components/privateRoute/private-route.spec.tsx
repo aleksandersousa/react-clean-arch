6e921fc0ac8275309d098b0c8bc5e5a7ab971958
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-29 23:12:05 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/privateRoute/private-route.spec.tsx
+++ src/presentation/components/privateRoute/private-route.spec.tsx
@@ -4,22 +4,25 @@ import { UtilsHelper } from '@/presentation/test';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 
+const makeSut = (): void => {
+  UtilsHelper.startInRoute('/');
+
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<div />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<div />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 describe('PrivateRoute', () => {
   test('should redirect to /login if token is empty', () => {
-    UtilsHelper.startInRoute('/');
-
-    render(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<div />} />
-
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<div />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-
+    makeSut();
     expect(window.location.pathname).toBe('/login');
   });
 });
