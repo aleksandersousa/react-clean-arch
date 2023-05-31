0ac8c0176ea8a3f5666a46ee142c11208dd3da46
aleksandersousa <aleksander-ssousa@outlook.com>
2023-05-31 19:02:10 -0300

    feat: ensure AuthorizeDecorator return the same as HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -19,8 +19,8 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
       });
     }
 
-    await this.httpGetClient.get(params);
+    const httpReponse = await this.httpGetClient.get(params);
 
-    return null;
+    return httpReponse;
   };
 }
