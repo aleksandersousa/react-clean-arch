dd6a30b6f2311ff2743b99ad8c720d22daa91b47
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-28 14:35:22 -0300

    feat: ensure SurveyResult render presents correct initial state

--- src/presentation/components/loading/Loading.tsx
+++ src/presentation/components/loading/Loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from '@/presentation/components';
 import Styles from './styles.scss';
 
 const Loading: React.FC = () => (
-  <div className={Styles.loadingWrap}>
+  <div data-testid="loading" className={Styles.loadingWrap}>
     <div className={Styles.loading}>
       <span>Aguarde...</span>
       <Spinner isNegative />
--- src/presentation/pages/survey-result/SurveyResult.tsx
+++ src/presentation/pages/survey-result/SurveyResult.tsx
@@ -1,47 +1,65 @@
 import FlipMove from 'react-flip-move';
-import { Calendar, Footer, Header, Loading } from '@/presentation/components';
+import { useState } from 'react';
+import { Calendar, Error, Footer, Header, Loading } from '@/presentation/components';
+import { SurveyModel } from '@/domain/models';
 import Styles from './styles.scss';
 
-const SurveyResult: React.FC = () => (
-  <div className={Styles.surveyResultWrap}>
-    <Header />
+const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as SurveyModel,
+  });
 
-    <div data-testid="content-wrap" className={Styles.contentWrap}>
-      {true && (
-        <>
-          <hgroup>
-            <Calendar date={new Date()} className={Styles.calendarWrap} />
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header />
 
-            <h2>Qual é seu framework web favorito?</h2>
-          </hgroup>
+      <div data-testid="survey-result" className={Styles.contentWrap}>
+        {state.surveyResult && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
 
-          <FlipMove className={Styles.answersList}>
-            <li>
-              <img src="" alt="" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li className={Styles.active}>
-              <img src="" alt="" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li>
-              <img src="" alt="" />
-              <span className={Styles.answer}>ReactJS</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-          </FlipMove>
+              <h2>Qual é seu framework web favorito?</h2>
+            </hgroup>
 
-          <button type="button">Voltar</button>
-        </>
-      )}
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img src="" alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img src="" alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li>
+                <img src="" alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
 
-      {false && <Loading />}
+            <button type="button">Voltar</button>
+          </>
+        )}
+
+        {state.isLoading && <Loading />}
+        {state.error && (
+          <Error
+            error={state.error}
+            reload={(): void => {
+              console.log('error');
+            }}
+          />
+        )}
+      </div>
+
+      <Footer />
     </div>
-
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default SurveyResult;
