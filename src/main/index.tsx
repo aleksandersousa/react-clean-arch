00c10a7d3fe80bb5076445d541b5772156b9a2a6
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-27 23:05:11 -0300

    refactor: change index to use createRoot

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+const root = createRoot(document.getElementById('main'));
+
+root.render(<App />);
