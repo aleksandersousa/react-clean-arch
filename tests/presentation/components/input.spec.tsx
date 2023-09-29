2b886d58a1e6febbef8eab047e47e65b7c1fa699
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-29 13:40:32 -0300

    fix: use correct test

--- tests/presentation/components/input.spec.tsx
+++ tests/presentation/components/input.spec.tsx
@@ -1,49 +1,24 @@
-import { BrowserRouter } from 'react-router-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
-import { mockAccountModel } from '@/tests/domain/mocks';
-import { UtilsHelper } from '@/tests/presentation/mocks';
-import { ApiContext } from '@/presentation/contexts';
-import { Header } from '@/presentation/components';
-import { AccountModel } from '@/domain/models';
+import { RenderResult, fireEvent, render } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
+import { FormContext } from '@/presentation/contexts';
+import { Input } from '@/presentation/components';
 
-type SutTypes = {
-  setCurrentAccountMock: (account: AccountModel) => void;
-};
-
-const makeSut = (account = mockAccountModel()): SutTypes => {
-  UtilsHelper.startInRoute('/');
-
-  const setCurrentAccountMock = jest.fn();
-
+const makeSut = (fieldName: string): RenderResult =>
   render(
-    <ApiContext.Provider
-      value={{
-        setCurrentAccount: setCurrentAccountMock,
-        getCurrentAccount: () => account,
-      }}
-    >
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    </ApiContext.Provider>,
+    <FormContext.Provider value={{ state: {} }}>
+      <Input name={fieldName} />
+    </FormContext.Provider>,
   );
 
-  return { setCurrentAccountMock };
-};
+describe('InputComponent', () => {
+  test('should focus input on label click', () => {
+    const field = faker.database.column();
+    const sut = makeSut(field);
 
-describe('Header Component', () => {
-  test('should call setCurrentAccount with null', () => {
-    const { setCurrentAccountMock } = makeSut();
-    fireEvent.click(screen.getByTestId('logout'));
+    const input = sut.getByTestId(field);
+    const label = sut.getByTestId(`${field}-label`);
+    fireEvent.click(label);
 
-    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined);
-    expect(window.location.pathname).toBe('/login');
-  });
-
-  test('should render username correctly', () => {
-    const account = mockAccountModel();
-    makeSut(account);
-
-    expect(screen.getByTestId('username')).toHaveTextContent(account.name);
+    expect(document.activeElement).toBe(input);
   });
 });
