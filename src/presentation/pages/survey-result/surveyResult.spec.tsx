756b0f8959c448ad5dcb5609d99b44539c428a77
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-29 01:57:36 -0300

    test: ensure SurveyResult present new Result on answer click

--- src/presentation/pages/survey-result/surveyResult.spec.tsx
+++ src/presentation/pages/survey-result/surveyResult.spec.tsx
@@ -187,6 +187,8 @@ describe('SurveyResult Component', () => {
     expect(saveSurveyResultSpy.params).toEqual({
       answer: loadSurveyResultSpy.surveyResult.answers[1].answer,
     });
+
+    await waitFor(() => screen.getByTestId('survey-result'));
   });
 
   test('Should render error on UnexpectedError in SaveSurveyResult', async () => {
@@ -199,7 +201,7 @@ describe('SurveyResult Component', () => {
     await waitFor(() => screen.getByTestId('survey-result'));
 
     const answersWrap = screen.queryAllByTestId('answer-wrap');
-    fireEvent.click(answersWrap[1]);
+    await waitFor(() => fireEvent.click(answersWrap[1]));
 
     await waitFor(() => screen.getByTestId('error'));
 
@@ -219,11 +221,50 @@ describe('SurveyResult Component', () => {
     await waitFor(() => screen.getByTestId('survey-result'));
 
     const answersWrap = screen.queryAllByTestId('answer-wrap');
-    fireEvent.click(answersWrap[1]);
-
-    await waitFor(() => screen.getByTestId('survey-result'));
+    await waitFor(() => fireEvent.click(answersWrap[1]));
 
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined);
     expect(window.location.pathname).toBe('/login');
+  });
+
+  test('Should present SurveyResult data on SaveSurveyResult success', async () => {
+    const surveyResult = {
+      ...mockSurveyResultModel(),
+      date: new Date('2018-02-20T00:00:00'),
+    };
+
+    const saveSurveyResultSpy = new SaveSurveyResultSpy();
+    saveSurveyResultSpy.surveyResult = surveyResult;
+
+    makeSut({ saveSurveyResultSpy });
+
+    await waitFor(() => screen.getByTestId('survey-result'));
+
+    const answersWrap = screen.queryAllByTestId('answer-wrap');
+    await waitFor(() => fireEvent.click(answersWrap[1]));
+
+    expect(screen.getByTestId('day')).toHaveTextContent('20');
+    expect(screen.getByTestId('month')).toHaveTextContent('fev');
+    expect(screen.getByTestId('year')).toHaveTextContent('2018');
+
+    expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question);
+    expect(screen.getByTestId('answers').childElementCount).toBe(2);
+
+    expect(answersWrap[0]).toHaveClass('active');
+    expect(answersWrap[1]).not.toHaveClass('active');
+
+    const images = screen.queryAllByTestId('image');
+    expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image);
+    expect(images[1]).toBeFalsy();
+
+    const answers = screen.queryAllByTestId('answer');
+    expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer);
+    expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer);
+
+    const percents = screen.queryAllByTestId('percent');
+    expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`);
+    expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`);
+
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
   });
 });
