ad5c669559a8a22fb08e3603431e8ad0d84841fa
aleksandersousa <aleksander-ssousa@outlook.com>
2023-09-28 22:45:52 -0300

    feat: ensure RemoteSaveSurveyResult calls HttpClient with correct values

---
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpStatusCode } from '@/data/protocols/http';
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
+import { SurveyResultModel } from '@/domain/models';
+import { SaveSurveyResult, SaveSurveyResultParams } from '@/domain/usecases';
+
+export class RemoteSaveSurveyResult implements SaveSurveyResult {
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient<SurveyResultModel>,
+  ) {}
+
+  async save(data: SaveSurveyResultParams): Promise<SurveyResultModel> {
+    const httpResponse = await this.httpClient.request({
+      url: this.url,
+      method: 'get',
+      body: data,
+    });
+    const surveyResult = httpResponse.body;
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return { ...surveyResult, date: new Date(surveyResult?.date) };
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError();
+      default:
+        throw new UnexpectedError();
+    }
+  }
+}
